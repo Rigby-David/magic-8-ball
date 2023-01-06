845c41c8584083474fdee6d43a7f6ee483f337d5
David <Rigby335@hotmail.com>
2023-01-06 11:10:30 -0800

    hard coding answers and verifying

--- app.js
+++ app.js
@@ -5,13 +5,13 @@ const questionInput = document.getElementById('question-input');
 const questionButton = document.getElementById('question-button');
 const answerOutput = document.getElementById('answer-output');
 
-console.log('questionInput', questionInput);
-console.log('questionButton', questionButton);
-console.log('answerOutput', answerOutput);
 /* State */
-
+const answers = ['Yes', 'No', 'Maybe', 'Try again', 'Decidedly so', 'Hahaha'];
 /* Events */
-
+questionButton.addEventListener('click', () => {
+    answerOutput.textContent = answers[0];
+    questionInput.value = '';
+});
 /* Display Functions */
 
 // (don't forget to call any display functions you want to run on page load!)
